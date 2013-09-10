2cf070a6ccc1d06de1ae838d3c155ffe776c4990
Akiyoshi Tanaka <akkunchoi@gmail.com>
2013-09-10 20:28:58 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Livestreaming::Application.config.secret_key_base = '50d31265166786838632a06ce37fc7decd461fb7ae0e5f99abd1ef8ef3bb315bf8d7f12816cefcda6e25d8512446d9a5d9aaee81453eaf52053594e78fc51757'
